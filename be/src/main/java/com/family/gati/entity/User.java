3b77ec2e67aaed447f9480b6e63580c12539bbaa
akdgns12 <akdgns12@naver.com>
2023-01-26 22:55:32 +0900

    [Modify] user 엔티티 nullable ->=fle=false -> @NotNull

--- be/src/main/java/com/family/gati/entity/User.java
+++ be/src/main/java/com/family/gati/entity/User.java
@@ -3,6 +3,7 @@ package com.family.gati.entity;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Entity
@@ -15,22 +16,28 @@ public class User {
     @GeneratedValue // 기본 키 자동 생성
     private Long user_seq;
 
-    @Column(name = "USER_ID", length = 20, nullable = false, unique = true)
+    @Column(name = "USER_ID", length = 20, unique = true)
+    @NotNull
     private String userId;
 
-    @Column(name = "EMAIL", length = 50, nullable = false)
+    @Column(name = "EMAIL", length = 50)
+    @NotNull
     private String email;
 
     @Column(name = "PASSWORD", columnDefinition = "CHAR(64)", length = 64, nullable = false)
+    @NotNull
     private String password;
 
-    @Column(name = "NICKNAME", length = 20, nullable = false)
+    @Column(name = "NICKNAME", length = 20)
+    @NotNull
     private String nickName;
 
-    @Column(name = "BIRTH", columnDefinition = "CHAR(8)", length = 8, nullable = false)
+    @Column(name = "BIRTH", columnDefinition = "CHAR(8)", length = 8)
+    @NotNull
     private String birth;
 
-    @Column(name = "PHONE_NUMBER", columnDefinition = "CHAR(11)", length = 11, nullable = false)
+    @Column(name = "PHONE_NUMBER", columnDefinition = "CHAR(11)", length = 11)
+    @NotNull
     private char phoneNumber;
 
     @Column(name = "MAIN_GROUP")
@@ -40,19 +47,23 @@ public class User {
     private int plusMinus;
 
     @Temporal(TemporalType.TIMESTAMP)
-    @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @NotNull
     private Date createTime;
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(columnDefinition = "TIMESTAMP DEFAULT NULL ON CURRENT_TIMESTAMP")
     private Date updateTime;
 
-    @Column(name = "ACCESS_TOKEN", length = 200, nullable = false)
+    @Column(name = "ACCESS_TOKEN", length = 200)
+    @NotNull
     private String accessToken;
 
-    @Column(name = "REFRESH_TOKEN", length = 200, nullable = false)
+    @Column(name = "REFRESH_TOKEN", length = 200)
+    @NotNull
     private String refreshToken;
 
-    @Column(name = "SALT", length = 64, nullable = false)
+    @Column(name = "SALT", length = 64)
+    @NotNull
     private String salt;
 }
