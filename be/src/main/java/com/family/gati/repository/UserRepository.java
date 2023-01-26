dd4ee8b3875f8809074a98dda841954ae14db3ee
akdgns12 <akdgns12@naver.com>
2023-01-26 19:42:43 +0900

    [Add] userRepository

--- be/src/main/java/com/family/gati/repository/UserRepository.java
+++ be/src/main/java/com/family/gati/repository/UserRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    User join(User user);
     User findByUserId(String userId);
     User findByUserSeq(Long userSeq);
+    User modify(User user);
+    User deleteByUserSeq(Long userSeq);
 }
