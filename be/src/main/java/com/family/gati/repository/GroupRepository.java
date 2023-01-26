b72f516d2465e95207f1fe2eeeaf2946ee8b04b8
akdgns12 <akdgns12@naver.com>
2023-01-26 23:15:56 +0900

    [Add] GroupRepository ì

---
+++ be/src/main/java/com/family/gati/repository/GroupRepository.java
@@ -0,0 +1,10 @@
+package com.family.gati.repository;
+
+import com.family.gati.entity.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+    List<Group> findByUserId(String userId);
+}
