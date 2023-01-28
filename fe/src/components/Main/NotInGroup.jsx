fc048778ec82792421957aaf502b82d64303cc6d
bkkmw <roboticts49@gmail.com>
2023-01-28 15:24:09 +0900

    [Design] not in group message color

--- fe/src/components/Main/NotInGroup.jsx
+++ fe/src/components/Main/NotInGroup.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 /** @jsxImportSource @emotion/react */
-import { jsx, css } from '@emotion/react';
+import { css } from '@emotion/react';
 import WarningAmberIcon from '@mui/icons-material/WarningAmber';
 import { Typography } from "@mui/material";
 
@@ -10,13 +10,16 @@ const contStyle = css`
   margin-left: auto;
   margin-right: auto;
   margin-top: 50%;
+  .head {
+    color: gray;
+  }
 `
 
 export default function NotInGroup() {
 
   return (
     <div css={contStyle}>
-      <div>
+      <div class="head">
         <WarningAmberIcon />
         <Typography>아직 속한 그룹이 없습니다.</Typography>
       </div>
