4f9426f3d2235c65390f071278c5d3863584bd13
bkkmw <roboticts49@gmail.com>
2023-01-28 17:34:38 +0900

    [Fix] remove top margin for reusability

--- fe/src/components/Main/NotInGroup.jsx
+++ fe/src/components/Main/NotInGroup.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
-import WarningAmberIcon from '@mui/icons-material/WarningAmber';
+import { css } from "@emotion/react";
+import WarningAmberIcon from "@mui/icons-material/WarningAmber";
 import { Typography } from "@mui/material";
 
 const contStyle = css`
@@ -9,24 +9,19 @@ const contStyle = css`
   width: 80%;
   margin-left: auto;
   margin-right: auto;
-  margin-top: 50%;
   .head {
     color: gray;
   }
-`
+`;
 
 export default function NotInGroup() {
-
   return (
     <div css={contStyle}>
       <div className="head">
         <WarningAmberIcon />
         <Typography>아직 속한 그룹이 없습니다.</Typography>
       </div>
-      <Typography>
-        나만의 가족 그룹을 만들고 <br />
-        가족들과의 추억을 저장하세요!
-      </Typography>
+      <Typography>나만의 가족 그룹을 만들고 가족들과의 추억을 저장하세요!</Typography>
     </div>
   );
-};
+}
