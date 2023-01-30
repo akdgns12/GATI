8c3e350690f5d1b76443f1655596b30b8477e6c7
unknown <misukj68@naver.com>
2023-01-30 17:43:19 +0900

    [Add] PhotoBook

--- fe/src/components/PhotoBook/DatePicker.jsx
+++ fe/src/components/PhotoBook/DatePicker.jsx
@@ -4,13 +4,22 @@ import TextField from '@mui/material/TextField';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { width } from '@mui/system';
+import { alignProperty } from '@mui/material/styles/cssUtils';
 
 export default function BasicDatePicker() {
   const [value, setValue] = React.useState(null);
 
   return (
+    <div
+      style={{
+        width:'30px',
+      }}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DatePicker
+          style={{
+            border: '0px'
+          }}
           value={value}
           onChange={(newValue) => {
             setValue(newValue);
@@ -18,5 +27,6 @@ export default function BasicDatePicker() {
           renderInput={(params) => <TextField {...params} />}
           />
       </LocalizationProvider>
+    </div>
   );
 }
--- fe/src/components/PhotoBook/SearchBar.jsx
+++ fe/src/components/PhotoBook/SearchBar.jsx
@@ -1,18 +1,35 @@
 import * as React from 'react';
-import Paper from '@mui/material/Paper';
+import { Paper, Box } from '@mui/material';
 import InputBase from '@mui/material/InputBase';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import { css } from "@emotion/react";
+import { borderRadius } from '@mui/system';
+
+
+const SearchBar = css`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px 11px;
+
+`;
 
 
 export default function CustomizedInputBase() {
   return (
-      <Paper
+      <container
         component="form"
-        sx={{mx: 'auto', p: '2px 4px', display: 'flex', alignItems: 'center', width: 400, }}
+        css={SearchBar}
+        style={{
+          margin: '20px',
+          width: '311px',
+          height: '40px',
+          backgroundColor: '#E8E8E8',
+          borderRadius: '8px'}}
       >
         <InputBase
           sx={{ ml: 1, flex: 1 }}
@@ -26,6 +43,6 @@ export default function CustomizedInputBase() {
         <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
           <CalendarMonthIcon />
         </IconButton> */}
-      </Paper>
+      </container>
   );
 }
