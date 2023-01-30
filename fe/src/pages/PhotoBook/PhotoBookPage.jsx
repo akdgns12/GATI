050a121060c4890387abae35d0089e622fffa579
unknown <misukj68@naver.com>
2023-01-30 17:09:32 +0900

    [Add] Photobook - searchbar, datepicker,imagelist

--- fe/src/pages/PhotoBook/PhotoBookPage.jsx
+++ fe/src/pages/PhotoBook/PhotoBookPage.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import CustomizedInputBase from "../../components/PhotoBook/SearchBar";
 import BasicDatePicker from "../../components/PhotoBook/DatePicker";
+import ImageList from '../../components/PhotoBook/ImageList'
+
 class PhotoBook extends React.Component {
   render() {
     return (
       <div>
-        <CustomizedInputBase/>
-        <BasicDatePicker/>
+        <div
+          style={{display:'flex', p:'20px'}}>
+          <CustomizedInputBase/>
+          <BasicDatePicker/>
+        </div>
+        <ImageList/>
       </div>
     )
   }
