f2a2f41decd29f9fe34fc718e45bdfab37d4050d
bkkmw <roboticts49@gmail.com>
2023-01-30 14:20:06 +0900

    [Add] Post detail page

--- fe/src/pages/Main/MainPage.jsx
+++ fe/src/pages/Main/MainPage.jsx
@@ -2,12 +2,14 @@ import { React, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MainFeed from './MainFeed';
 import CreatePost from './CreatePost';
+import PostDetail from './PostDetail';
 
 const Home = (props) => {
   return (
     <Routes>
       <Route path='/' element={<MainFeed />} />
       <Route path='/post' element={<CreatePost />} />
+      <Route path='/detail/:postId' element={<PostDetail />} />
     </Routes>
   );
 };
