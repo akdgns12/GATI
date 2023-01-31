f09cafbe061784a5a7fcd6a55a0ff2aaa528283a
seotai78@naver.com <seotai78@naver.com>
2023-01-31 18:01:53 +0900

    [Add] calendar-plan

--- fe/src/components/Calendar/Plans.jsx
+++ fe/src/components/Calendar/Plans.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import {React, useState}  from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import PlanCard from './PlanCard';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 export default function Plans() {
+  const {planData} = useSelector((state) => state.schedule)
+
   return (
-    <div>Plans</div>
+    <div>
+      {planData.map((plan, index) => {
+        return <PlanCard key={index} plan={plan}/>
+      })}
+    </div>
   )
+  
 }
