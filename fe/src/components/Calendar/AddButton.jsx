63740406dbf3eb091c48bd39858cd46865cb6c29
seotai78@naver.com <seotai78@naver.com>
2023-02-01 17:21:43 +0900

    [Add] 가치가자 레이아웃 ~ing

--- fe/src/components/Calendar/AddButton.jsx
+++ fe/src/components/Calendar/AddButton.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
+import { Opacity } from '@mui/icons-material';
 
 
 export default class addButton extends Component {
@@ -10,11 +11,11 @@ export default class addButton extends Component {
       <Box sx={{ '& > :not(style)': { m: 1 } }}>
         <Fab
          size="small" 
-         color='secondary'
          aria-label="add"
          style={{
           zIndex: "1000", width: "60px", height: "60px",
           position: "fixed", right: "20px", bottom: "60px",
+          backgroundColor: '#0095FF4D',
         }}
          >
           <AddIcon />
--- fe/src/pages/GoTogether/GoTogetherPage.jsx
+++ fe/src/pages/GoTogether/GoTogetherPage.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom/client";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
-class GoTogether extends React.Component {
-  render() {
-    return <h1>GoTogether</h1>;
-  }
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+export default function GoTogether() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  };
+  return (
+    <Grid>
+      <Typography variant="h4" sx={{display: 'flex', justifyContent: 'start', m: 2}}>
+        도시명
+      </Typography>
+      <Slider {...settings}>
+          <Box>
+            <Typography>1</Typography>
+          </Box>
+          <Box>
+            <Typography>2</Typography>
+          </Box>
+          <Box>
+            <Typography>3</Typography>
+          </Box>
+          <Box>
+            <Typography>4</Typography>
+          </Box>
+        </Slider>
+    </Grid>
+  )
 }
 
-export default GoTogether;
