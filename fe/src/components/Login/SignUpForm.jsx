ee1c7e00950f9f28c9ed891a37bc759b79f2adda
bkkmw <roboticts49@gmail.com>
2023-02-01 14:49:43 +0900

    [Feat] previous button

--- fe/src/components/Login/SignUpForm.jsx
+++ fe/src/components/Login/SignUpForm.jsx
@@ -2,19 +2,35 @@ import React, { useState, useRef } from "react";
 import ReactDOM from "react-dom/client";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { Container, Box, TextField, Button, Input, FormHelperText, InputLabel, ToggleButtonGroup, ToggleButton } from "@mui/material";
+import {
+  Container,
+  Box,
+  TextField,
+  Button,
+  InputLabel,
+  ToggleButtonGroup,
+  ToggleButton,
+} from "@mui/material";
+
+import { useNavigate } from "react-router";
 
 const contStyle = css`
   max-width: 300px;
   width: 100%;
   margin-top: 33vh;
-  background-color: rgba(192, 192, 192, 0.75);
+  background-color: rgba(255, 255, 255, 0.75);
   .form-box {
     width: 100%;
-    justify-content: center;
-    .labeled-box{
+    .header {
+      display: flex;
+      justify-content: center;
+      .title-box {
+        display: inline-block;
+      }
+    }
+    .labeled-box {
       margin-top: 8px;
-      .label-box{
+      .label-box {
         display: flex;
         justify-content: space-between;
         .form-label {
@@ -26,20 +42,26 @@ const contStyle = css`
       .text-form {
         margin-top: 2px;
       }
-      .double-box{
+      .double-box {
         display: flex;
         justify-content: space-between;
-        .date-form{
+        .date-form {
           width: 55%;
         }
-        .cal-btn{
+        .cal-btn {
           width: 40%;
           max-height: inherit;
         }
       }
     }
-    .submit-btn {
-      margin-top : 10px;
+    .btn-group {
+      margin-top: 10px;
+      .submit-btn {
+        margin: 5px;
+      }
+      .prev-btn {
+        margin: 5px;
+      }
     }
   }
 `;
@@ -52,6 +74,8 @@ const SignUpForm = () => {
   const [validEmail, setValidEmail] = useState(false);
   const [calType, setCalType] = useState("gregorian");
   const [isLast, setIsLast] = useState(false);
+
+  const navigate = useNavigate();
 
   const inputRef = useRef([]);
 
@@ -66,10 +90,8 @@ const SignUpForm = () => {
   function checkPW(event) {
     const inputPW = event.target.value;
     setCurrentPW(inputPW);
-    if (inputPW.length < 5)
-      setValidPW(false);
-    else
-      setValidPW(true);
+    if (inputPW.length < 5) setValidPW(false);
+    else setValidPW(true);
   }
 
   function comparePW(event) {
@@ -77,8 +99,7 @@ const SignUpForm = () => {
     // console.log(inputRef.current[1]);
     if (currentPW === PWConfirm) {
       setCheckedPW(true);
-    }
-    else setCheckedPW(false);
+    } else setCheckedPW(false);
   }
 
   function checkEmail(event) {
@@ -86,16 +107,14 @@ const SignUpForm = () => {
     const email = event.target.value;
     if (emailRegex.test(email)) {
       setValidEmail(true);
-    }
-    else setValidEmail(false);
+    } else setValidEmail(false);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
     if (!isLast) {
       toNext();
-    }
-    else {
+    } else {
       alert("Registered");
     }
   }
@@ -105,7 +124,8 @@ const SignUpForm = () => {
   }
 
   function toPrev() {
-    setIsLast(false);
+    if (isLast) setIsLast(false);
+    else navigate('/login');
   }
 
   function handleCalType(event, newType) {
@@ -115,34 +135,50 @@ const SignUpForm = () => {
   return (
     <Container css={contStyle}>
       <Box component="form" className="form-box" onSubmit={handleSubmit}>
-        회원가입
-        <Box className="labeled-box" style={{ display: isLast ? 'none' : 'block' }}>
+        <Box className="header">
+          <div className="title-box">회원가입</div>
+        </Box>
+        <Box
+          className="labeled-box"
+          style={{ display: isLast ? "none" : "block" }}
+        >
           <Box className="label-box">
             <InputLabel className="form-label">ID</InputLabel>
-            <span className="check-msg" style={{ color: validID ? 'green' : 'red' }}>
-              {validID ? ("사용 가능한 ID 입니다.") : ("사용 불가능한 ID 입니다.")}
+            <span
+              className="check-msg"
+              style={{ color: validID ? "green" : "red" }}
+            >
+              {validID ? "사용 가능한 ID 입니다." : "사용 불가능한 ID 입니다."}
             </span>
           </Box>
           <TextField
             fullWidth
             className="text-form"
-            ref={(el) => inputRef.current[0] = el}
+            ref={(el) => (inputRef.current[0] = el)}
             placeholder="ID"
             // error={!validID}
             onChange={checkID}
           />
         </Box>
-        <Box className="labeled-box" style={{ display: isLast ? 'none' : 'block' }}>
+        <Box
+          className="labeled-box"
+          style={{ display: isLast ? "none" : "block" }}
+        >
           <Box className="label-box">
             <InputLabel className="form-label">비밀번호</InputLabel>
-            <span className="check-msg" style={{ color: validPW ? 'green' : 'red' }}>
-              {checkPW ? ("사용 가능한 비밀번호 입니다.") : ("사용 불가능한 ID 입니다.")}
+            <span
+              className="check-msg"
+              style={{ color: validPW ? "green" : "red" }}
+            >
+              {checkPW
+                ? "사용 가능한 비밀번호 입니다."
+                : "사용 불가능한 ID 입니다."}
             </span>
           </Box>
           <TextField
             fullWidth
             className="text-form"
-            ref={(el) => inputRef.current[1] = el}
+            ref={(el) => (inputRef.current[1] = el)}
             type="password"
             name="PW"
             placeholder="Password"
@@ -150,13 +186,19 @@ const SignUpForm = () => {
             onChange={checkPW}
           />
         </Box>
-        <Box className="labeled-box" style={{ display: isLast ? 'none' : 'block' }}>
+        <Box
+          className="labeled-box"
+          style={{ display: isLast ? "none" : "block" }}
+        >
           <Box className="label-box">
             <InputLabel className="form-label">비밀번호 확인</InputLabel>
-            <span className="check-msg" style={{ color: checkedPW ? 'green' : 'red' }}>
-              {
-                checkedPW ? ("비밀번호가 일치합니다") : ("비밀번호가 일치하지 않습니다.")
-              }
+            <span
+              className="check-msg"
+              style={{ color: checkedPW ? "green" : "red" }}
+            >
+              {checkedPW
+                ? "비밀번호가 일치합니다"
+                : "비밀번호가 일치하지 않습니다."}
             </span>
           </Box>
           <TextField
@@ -169,13 +211,19 @@ const SignUpForm = () => {
             onChange={comparePW}
           />
         </Box>
-        <Box className="labeled-box" style={{ display: isLast ? 'none' : 'block' }}>
+        <Box
+          className="labeled-box"
+          style={{ display: isLast ? "none" : "block" }}
+        >
           <Box className="label-box">
             <InputLabel className="form-label">E-mail</InputLabel>
-            <span className="check-msg" style={{ color: validEmail ? 'green' : 'red' }}>
-              {
-                validEmail ? ("유효한 Email 입니다.") : ("유효하지 않은 형식입니다.")
-              }
+            <span
+              className="check-msg"
+              style={{ color: validEmail ? "green" : "red" }}
+            >
+              {validEmail
+                ? "유효한 Email 입니다."
+                : "유효하지 않은 형식입니다."}
             </span>
           </Box>
           <TextField
@@ -189,12 +237,15 @@ const SignUpForm = () => {
           />
         </Box>
 
-        <Box className="labeled-box" style={{ display: isLast ? 'block' : 'none' }}>
+        <Box
+          className="labeled-box"
+          style={{ display: isLast ? "block" : "none" }}
+        >
           <Box className="label-box">
             <InputLabel className="form-label">이름</InputLabel>
           </Box>
           <TextField
-            required={(isLast) ? true : false}
+            required={isLast ? true : false}
             fullWidth
             className="text-form"
             type="text"
@@ -202,12 +253,15 @@ const SignUpForm = () => {
             placeholder="Nickname"
           />
         </Box>
-        <Box className="labeled-box" style={{ display: isLast ? 'block' : 'none' }}>
+        <Box
+          className="labeled-box"
+          style={{ display: isLast ? "block" : "none" }}
+        >
           <Box className="label-box">
             <InputLabel className="form-label">전화번호</InputLabel>
           </Box>
           <TextField
-            required={(isLast) ? true : false}
+            required={isLast ? true : false}
             fullWidth
             className="text-form"
             type="number"
@@ -215,13 +269,16 @@ const SignUpForm = () => {
             placeholder="number"
           />
         </Box>
-        <Box className="labeled-box" style={{ display: isLast ? 'block' : 'none' }}>
+        <Box
+          className="labeled-box"
+          style={{ display: isLast ? "block" : "none" }}
+        >
           <Box className="label-box">
             <InputLabel className="form-label">생일</InputLabel>
           </Box>
           <Box className="double-box">
             <TextField
-              required={(isLast) ? true : false}
+              required={isLast ? true : false}
               fullWidth
               className="date-form"
               type="date"
@@ -240,19 +297,25 @@ const SignUpForm = () => {
           </Box>
         </Box>
 
-        <Button
-          variant="outlined"
-          color="primary"
-          type="submit"
-          className="submit-btn"
-          disabled={!validID || !validPW || !checkedPW || !validEmail}
-        >
-          {
-            isLast
-              ? "완료"
-              : "다음"
-          }
-        </Button>
+        <Box className="btn-group">
+          <Button
+            variant="outlined"
+            color="primary"
+            className="prev-btn"
+            onClick={toPrev}
+          >
+            이전
+          </Button>
+          <Button
+            variant="outlined"
+            color="primary"
+            type="submit"
+            className="submit-btn"
+            disabled={!validID || !validPW || !checkedPW || !validEmail}
+          >
+            {isLast ? "완료" : "다음"}
+          </Button>
+        </Box>
       </Box>
     </Container>
   );
