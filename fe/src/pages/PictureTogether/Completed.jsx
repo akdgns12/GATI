fce2afdcee426efe518d6aef2520100b448fcc61
unknown <misukj68@naver.com>
2023-02-01 21:51:48 +0900

    [Add]pictures Together 깡통 만들기

---
+++ fe/src/pages/PictureTogether/Completed.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+export default function Completed() {
+    return <h1>Completed</h1>;
+};
--- fe/src/pages/PictureTogether/PictureTogetherPage.jsx
+++ fe/src/pages/PictureTogether/PictureTogetherPage.jsx
@@ -1,10 +1,54 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import * as React from 'react';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import InProgress from './InProgress';
+import Completed from './Completed';
 
-class PictureTogether extends React.Component {
-  render() {
-    return <h1>picturetogether</h1>;
+export default function PictureTogether() {
+  const [mode, setMode] = React.useState('inprogress');
+
+  const handleChange = (event, newMode) => {
+    setMode(newMode);
+  };
+  
+  let content = null
+  if (mode === 'inprogress') {
+    content = <InProgress />
+  } else {
+    content = <Completed />
   }
-}
 
-export default PictureTogether;
+  return (
+    <div
+      style={{
+        display:'flex',
+        flexDirection:'column',
+        alignContent:'center'
+      }}>
+      <div>
+        <ToggleButtonGroup
+          color="primary"
+          value={mode}
+          exclusive
+          onChange={handleChange}
+          aria-label="Platform"
+          style={{
+            flex:'auto',
+            margin:'30px',
+            width:'80vw'
+          }}
+          >
+          <ToggleButton value="inprogress" style={{ flex:1, height:'40px'}}>진행 중</ToggleButton>
+          <ToggleButton value="completed" style={{ flex:1, height:'40px'}}>완료</ToggleButton>
+        </ToggleButtonGroup>
+      </div>
+      <div
+        style={{
+          display:'flex',
+          justifyContent:'center',
+        }}>
+        {content}
+      </div>
+    </div>
+  );
+}
