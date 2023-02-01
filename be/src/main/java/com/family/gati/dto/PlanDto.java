224f0802b024f6ed848aa599fdce081268aa6da9
Ham <gkagu12@naver.com>
2023-02-01 12:25:09 +0900

    temporary

--- be/src/main/java/com/family/gati/dto/PlanDto.java
+++ be/src/main/java/com/family/gati/dto/PlanDto.java
@@ -1,6 +1,7 @@
 package com.family.gati.dto;
 
 import com.family.gati.entity.Plan;
+import com.family.gati.entity.User;
 import com.family.gati.util.CommonBuilder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,6 +30,32 @@ public class PlanDto {
     }
 
     public static class PlanDtoBuilder implements CommonBuilder<PlanDto> {
+        private Integer id;
+        private User user;
+        private Integer groupId;
+        private String title;
+        private Date startDate;
+        private Date endDate;
+        private String place;
+        private String memo;
+        private Date createTime;
+        private Date updateTime;
 
+        public PlanDtoBuilder(Plan plan){
+            this.id = plan.getId();
+            this.user = plan.getUser();
+            this.groupId = plan.getGroupId();
+            this.title = plan.getTitle();
+            this.startDate = plan.getStartDate();
+            this.endDate = plan.getEndDate();
+            this.place = plan.getPlace();
+            this.memo = plan.getMemo();
+            this.createTime = plan.getCreateTime();
+            this.updateTime = plan.getUpdateTime();
+        }
+        @Override
+        public PlanDto build(){
+            return new PlanDto(this);
+        }
     }
 }
