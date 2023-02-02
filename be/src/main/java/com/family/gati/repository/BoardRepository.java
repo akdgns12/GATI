ce2e0d18f865e00931dfa339bec16daa5e15869f
kimhero <duddnd180@naver.com>
2023-02-02 09:47:33 +0900

    [Add] Board repository

---
+++ be/src/main/java/com/family/gati/repository/BoardRepository.java
@@ -0,0 +1,12 @@
+package com.family.gati.repository;
+
+import com.family.gati.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Integer> {
+    List<Board> findByGroupId(Integer groupId);
+}
