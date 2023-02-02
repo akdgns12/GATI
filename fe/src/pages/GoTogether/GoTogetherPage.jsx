2fcc4abc0990c469d650bbeb36c154a721fdbd0c
seotai78@naver.com <seotai78@naver.com>
2023-02-02 14:40:21 +0900

    [Add] 가치가자 레이아웃

--- fe/src/pages/GoTogether/GoTogetherPage.jsx
+++ fe/src/pages/GoTogether/GoTogetherPage.jsx
@@ -7,35 +7,65 @@ import Typography from '@mui/material/Typography';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import City from "../../components/GoTogether/City";
+import { useSelector } from "react-redux";
+
 
 export default function GoTogether() {
+  
+  const { cities } = useSelector((state) => state.city)
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          // 한페이지에 몇장 띄울건지 정해주는 값
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      }
+    ]
   };
   return (
-    <Grid>
-      <Typography variant="h4" sx={{display: 'flex', justifyContent: 'start', m: 2}}>
-        도시명
-      </Typography>
+    <div>
+      <h1>서울</h1>
       <Slider {...settings}>
-          <Box>
-            <Typography>1</Typography>
-          </Box>
-          <Box>
-            <Typography>2</Typography>
-          </Box>
-          <Box>
-            <Typography>3</Typography>
-          </Box>
-          <Box>
-            <Typography>4</Typography>
-          </Box>
-        </Slider>
-    </Grid>
+        {cities.map((city, index) => {
+          if (city.cityname === '서울')
+            return <City key={index} city={city}/>
+        })}
+      </Slider>
+      <h1>부산</h1>
+      <Slider {...settings}>
+        {cities.map((city, index) => {
+          if (city.cityname === '부산')
+            return <City key={index} city={city}/>
+        })}
+      </Slider>
+    </div>
   )
 }
 
--- fe/src/store/index.js
+++ fe/src/store/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import board from "./Board/board.js";
 import noti from "./Nofitication/noti.js";
 import schedule from "./Schedule/schedule.js";
+import city from "./GoTogether/city.js";
 
 const rootReducer = combineReducers({
   board,
   noti,
   schedule,
+  city,
 });
 
 export default rootReducer;
