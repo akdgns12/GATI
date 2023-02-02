a7debb593017ca76aa7c6f2e4121487a68ffd42f
kimhero <duddnd180@naver.com>
2023-02-02 09:55:44 +0900

    [Add] BoardLikes entity

---
+++ be/src/main/java/com/family/gati/entity/BoardLikes.java
@@ -0,0 +1,26 @@
+package com.family.gati.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class BoardLikes {
+    @Id
+    @Column(name = "ID", nullable = false)
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "USER_ID", nullable = false, length = 20)
+    private String userId;
+
+    @Column(name = "BOARD_ID", nullable = false, length = 20)
+    private Integer boardId;
+}
