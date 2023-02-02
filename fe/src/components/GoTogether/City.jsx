6bd0c49323934dea91a1a33c10fc6205e2ca723a
seotai <gurwn9448@gmail.com>
2023-02-03 01:16:21 +0900

    [Delete] not using css file

--- fe/src/components/GoTogether/City.jsx
+++ fe/src/components/GoTogether/City.jsx
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import "./City.css";
+import Container from '@mui/material/Container';
 export default function City(props) {
   // react-slick 설정값
   
