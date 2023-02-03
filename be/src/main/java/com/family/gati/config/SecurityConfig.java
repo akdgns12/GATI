b62c54f5834304408df4a2d722d21de40c7d7078
akdgns12 <akdgns12@naver.com>
2023-02-03 16:19:11 +0900

    [Modify] cors configure

--- be/src/main/java/com/family/gati/config/SecurityConfig.java
+++ be/src/main/java/com/family/gati/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    private final CustomUserDetails customUserDetails;
     private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
     private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
-//    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
     private final CookieAuthorizationRequestRepository cookieAuthorizationRequestRepository;
@@ -93,39 +93,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .successHandler(oAuth2AuthenticationSuccessHandler)
                 .failureHandler(oAuth2AuthenticationFailureHandler);
 
-//        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-
-//        http
-//                .httpBasic().disable() // rest api이므로 기본설정 미사용
-//                .cors().and()   // CORS on
-//                .csrf().disable() // rest api이므로 csrf 보안 미사용
-//                .formLogin().disable()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // jwt로 인증하므로 세션 미사용
-//                .and()
-//                .oauth2Login()
-//                // 프론트엔드에서 백엔드로 소셜로그인 요청을 보내는 URI
-//                .authorizationEndpoint()
-//                .baseUri("/oauth2/authorization")
-//                .authorizationRequestRepository(cookieAuthorizationRequestRepository) // Authorization 과정에서 Cookie로 변경
-//                .and()
-//                // 인증 과정이 끝나면 인증 code와 함께 리다이렉트할 URI
-//                .redirectionEndpoint()
-//                .baseUri("login/oauth2/callback/*")
-//                .and()
-//                // Provider로부터 획득한 유저정보를 다룰 service class 지정
-//                .userInfoEndpoint()
-//                .userService(customOAuth2UserService)
-//                .and()
-//                // OAuth2 로그인 성공,실패시 호출할 handler
-//                .successHandler(oAuth2AuthenticationSuccessHandler)
-//                .failureHandler(oAuth2AuthenticationFailureHandler);
-//
-//        http.exceptionHandling()
-//                .authenticationEntryPoint(jwtAuthenticationEntryPoint) // 401
-//                .accessDeniedHandler(jwtAccessDeniedHandler); // 403
-//        // 모든 request에서 JWT를 검사할 filter 추가
-//        // UsernamePasswordAuthenticationFilter에서 클라이언트가 요청한 리소스의 접근권한이 없을때 막는 역할을 하기 때문에 이 필터 전에 jwtAuthenticationFilter 실행
-//        http.
-//                addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
--- be/src/main/java/com/family/gati/config/WebConfig.java
+++ be/src/main/java/com/family/gati/config/WebConfig.java
@@ -1,21 +1,33 @@
 package com.family.gati.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // SecurityConfig에서 CORS를 허용했기 때문에 관련설정 추가
 public class WebConfig implements WebMvcConfigurer {
 
-    private final long MAX_AGE_SECS = 3600;
+    /**
+     * CORS 설정, 허락된 URL, 허락된 Origin
+     * @return
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods(
-                        "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(MAX_AGE_SECS);
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*", "localhost:3000")
+                        .allowedMethods(
+                                HttpMethod.GET.name(),
+                                HttpMethod.HEAD.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.DELETE.name()
+                        );
+            }
+        };
     }
 }
