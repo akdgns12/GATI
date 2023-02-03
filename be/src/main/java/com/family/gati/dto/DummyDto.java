645e3f19e16b7adee0df96292809f8d27f438d40
hhj <gkagu12@naver.com>
2023-02-03 09:26:46 +0900

    dummyDto add

---
+++ be/src/main/java/com/family/gati/dto/DummyDto.java
@@ -0,0 +1,12 @@
+package com.family.gati.dto;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class DummyDto {
+    private int id;
+    private String userId;
+
+}
