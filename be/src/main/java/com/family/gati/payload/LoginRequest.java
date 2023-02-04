d6b545ffdd5422af703531c635608e39386aa11f
akdgns12 <akdgns12@naver.com>
2023-02-04 15:05:31 +0900

    [Modify] LoginRequest

--- be/src/main/java/com/family/gati/payload/LoginRequest.java
+++ be/src/main/java/com/family/gati/payload/LoginRequest.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 
 @Data
 @Getter @Setter
-public class LoginRequest { // oauth 회원관리 용도 -> 일반 로그인 용도로 변경(oauth 임시 생략)
-    String userId;
+public class LoginRequest { // oauth 회원관리 용도
+    String id;
     String password;
 }
