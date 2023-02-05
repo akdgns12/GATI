14a7ee1c68f9f6c1b5b1acee3d48f83760f84093
kimhero <duddnd180@naver.com>
2023-02-06 08:49:23 +0900

    [Add] MissionImage dto

--- be/src/main/java/com/family/gati/entity/MissionImage.java
+++ be/src/main/java/com/family/gati/entity/MissionImage.java
@@ -1,5 +1,7 @@
 package com.family.gati.entity;
 
+import com.family.gati.dto.MissionImageDto;
+import com.family.gati.util.CommonBuilder;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,4 +28,27 @@ public class MissionImage {
 
     @Column(name = "IMG", nullable = false, length = 200)
     private String img;
+
+    private MissionImage(MissionImage.MissionImageBuilder builder) {
+        this.id = builder.id;
+        this.groupId = builder.groupId;
+        this.userId = builder.userId;
+        this.img = builder.img;
+    }
+
+    public static class MissionImageBuilder implements CommonBuilder<MissionImage> {
+        private Integer id;
+        private Integer groupId;
+        private String userId;
+        private String img;
+
+        public MissionImageBuilder(MissionImageDto missionImageDto) {
+            this.id = missionImageDto.getId();
+            this.groupId = missionImageDto.getGroupId();
+            this.userId = missionImageDto.getUserId();
+            this.img = missionImageDto.getImg();
+        }
+
+        public MissionImage build() {return new MissionImage(this);}
+    }
 }
