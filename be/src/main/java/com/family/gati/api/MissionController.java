44ffdaa90370a0de0575dae80cefc54c19b380fb
kimhero <duddnd180@naver.com>
2023-02-06 09:27:27 +0900

    [Feat] missionImage CRUD

--- be/src/main/java/com/family/gati/api/MissionController.java
+++ be/src/main/java/com/family/gati/api/MissionController.java
@@ -1,6 +1,8 @@
 package com.family.gati.api;
 
 import com.family.gati.dto.MissionDto;
+import com.family.gati.dto.MissionImageDto;
+import com.family.gati.service.MissionImageService;
 import com.family.gati.service.MissionService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiParam;
@@ -16,6 +18,7 @@ import java.util.List;
 @Api(tags = "Mission API")
 public class MissionController {
     private final MissionService missionService;
+    private final MissionImageService missionImageService;
 
     @GetMapping("/{groupId}")
     public ResponseEntity<?> getMissionsByGroupId(@ApiParam(value = "path로 groupId 전달받음")@PathVariable("groupId") Integer groupId) {
@@ -44,6 +47,30 @@ public class MissionController {
     @DeleteMapping("/mission/{id}")
     public ResponseEntity<?> deleteMission(@ApiParam(value = "삭제 할 missionId")@PathVariable("id") Integer id) {
         missionService.deleteMissionById(id);
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/image/{groupId}")
+    public ResponseEntity<?> getMissionImagesByGroupId(@ApiParam(value = "path로 groupId 전달받음")@PathVariable("groupId") Integer groupId) {
+        List<MissionImageDto> resultDtos = missionImageService.findByGroupId(groupId);
+        return ResponseEntity.ok(resultDtos);
+    }
+
+    @PostMapping("/image")
+    public ResponseEntity<?> addMissionImage(@RequestBody MissionImageDto missionImageDto) {
+        MissionImageDto resultDto = missionImageService.insertMissionImage(missionImageDto);
+        return ResponseEntity.ok(resultDto);
+    }
+
+    @PutMapping("/image")
+    public ResponseEntity<?> updateMissionImage(@RequestBody MissionImageDto missionImageDto) {
+        MissionImageDto resultDto = missionImageService.updateMissionImage(missionImageDto);
+        return ResponseEntity.ok(resultDto);
+    }
+
+    @DeleteMapping("/image/{id}")
+    public ResponseEntity<?> deleteMissionImage(@ApiParam(value = "삭제 할 missionImageId")@PathVariable("id") Integer id) {
+        missionImageService.deleteMissionImageById(id);
         return ResponseEntity.ok(null);
     }
 }
