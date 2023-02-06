c8c45435559562e6a9bc73ad0f2ef08fee8e1849
kimhero <duddnd180@naver.com>
2023-02-06 17:34:04 +0900

    [Feat] Board 관련 Swagger 수정

--- be/src/main/java/com/family/gati/api/BoardController.java
+++ be/src/main/java/com/family/gati/api/BoardController.java
@@ -1,16 +1,15 @@
 package com.family.gati.api;
 
-import com.family.gati.dto.BoardCommentDto;
-import com.family.gati.dto.BoardDto;
+import com.family.gati.dto.*;
 import com.family.gati.service.BoardCommentService;
 import com.family.gati.service.BoardService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -24,6 +23,26 @@ public class BoardController {
     @ApiOperation(
             value = "현재 그룹의 Board 조회"
             , notes = "GroupId를 통해 현재 그룹의 Board를 최신순으로 조회한다.")
+    @ApiResponses({
+            @ApiResponse(
+                    code = 200
+                    , message = "조회 성공"
+                    , response = BoardDto.class
+                    , responseContainer = "List"
+            )
+//            , @ApiResponse(
+//            code = 201
+//            , message = "생성된 자원 정보"
+//            , response = ResponseDTO.class
+//            , responseContainer = "List"
+//    )
+//            , @ApiResponse(
+//            code = 409
+//            , message = "로직 수행 불가 모순 발생"
+//            , response = ErrorDTO.class
+//            , responseContainer = "List"
+//    )
+    })
     @GetMapping("/{groupId}")
     public ResponseEntity<?> getBoardsByGroupId(@ApiParam(value = "path로 groupId 전달받음")@PathVariable("groupId") Integer groupId) {
         List<BoardDto> findDtos = boardService.findByGroupId(groupId);
@@ -52,16 +71,52 @@ public class BoardController {
             value = "Board 작성"
             , notes = "Board를 작성한다.")
     @PostMapping("/board")
-    public ResponseEntity<?> addBoard(@RequestBody BoardDto boardDto) {
+    public ResponseEntity<?> addBoard(@RequestBody BoardRegistDto boardRegistDto) {
+        BoardDto boardDto = new BoardDto();
+        boardDto.setGroupId(boardRegistDto.getGroupId());
+        boardDto.setContent(boardRegistDto.getContent());
+        boardDto.setTag(boardRegistDto.getTag());
+        boardDto.setImg(boardRegistDto.getImg());
+        boardDto.setLikes(0);
+        boardDto.setComments(0);
+        boardDto.setCreateTime(new Timestamp(new Date().getTime()));
+        boardDto.setUpdateTime(new Timestamp(new Date().getTime()));
+
+        // 추후 jwt 활용
+        boardDto.setUserId("userId");
+        boardDto.setNickname("nickName");
+
         BoardDto resultDto = boardService.insertBoard(boardDto);
         return ResponseEntity.ok(resultDto);
     }
+
+//    기존 addboard
+//    @PostMapping("/board2")
+//    public ResponseEntity<?> addBoard2(@RequestBody BoardDto boardDto) {
+//        BoardDto resultDto = boardService.insertBoard(boardDto);
+//        return ResponseEntity.ok(resultDto);
+//    }
 
     @ApiOperation(
             value = "Board 수정"
             , notes = "Board를 수정한다.")
     @PutMapping("/board")
-    public ResponseEntity<?> updateBoard(@RequestBody BoardDto boardDto){
+    public ResponseEntity<?> updateBoard(@RequestBody BoardUpdateDto boardUpdateDto){
+        BoardDto boardDto = new BoardDto();
+        boardDto.setId(boardUpdateDto.getId());
+        boardDto.setGroupId(boardUpdateDto.getGroupId());
+        boardDto.setContent(boardUpdateDto.getContent());
+        boardDto.setTag(boardUpdateDto.getTag());
+        boardDto.setImg(boardUpdateDto.getImg());
+        boardDto.setLikes(0);
+        boardDto.setComments(0);
+        boardDto.setCreateTime(new Timestamp(new Date().getTime()));
+        boardDto.setUpdateTime(new Timestamp(new Date().getTime()));
+
+        // 추후 jwt 활용
+        boardDto.setUserId("userId");
+        boardDto.setNickname("nickName");
+
         BoardDto resultDto = boardService.updateBoard(boardDto);
         return ResponseEntity.ok(resultDto);
     }
@@ -70,7 +125,16 @@ public class BoardController {
             value = "Comment 작성"
             , notes = "Board의 Comment를 작성한다.")
     @PostMapping("/comment")
-    public ResponseEntity<?> addBoardComment(@RequestBody BoardCommentDto boardCommentDto) {
+    public ResponseEntity<?> addBoardComment(@RequestBody BoardCommentRegistDto boardCommentRegistDto) {
+        BoardCommentDto boardCommentDto = new BoardCommentDto();
+        boardCommentDto.setBoardId(boardCommentRegistDto.getBoardId());
+        boardCommentDto.setContent(boardCommentRegistDto.getContent());
+        boardCommentDto.setCreateTime(new Timestamp(new Date().getTime()));
+        boardCommentDto.setUpdateTime(new Timestamp(new Date().getTime()));
+        // 추후 jwt 활용
+        boardCommentDto.setUserId("userId");
+        boardCommentDto.setNickname("nickName");
+
         BoardCommentDto resultDto = boardCommentService.insertBoardComment(boardCommentDto);
         return ResponseEntity.ok(resultDto);
     }
@@ -79,7 +143,17 @@ public class BoardController {
             value = "Comment 수정"
             , notes = "Board의 Comment를 수정한다.")
     @PutMapping("/comment")
-    public ResponseEntity<?> updateBoardComment(@RequestBody BoardCommentDto boardCommentDto){
+    public ResponseEntity<?> updateBoardComment(@RequestBody BoardCommentUpdateDto boardCommentUpdateDto){
+        BoardCommentDto boardCommentDto = new BoardCommentDto();
+        boardCommentDto.setId(boardCommentUpdateDto.getId());
+        boardCommentDto.setBoardId(boardCommentUpdateDto.getBoardId());
+        boardCommentDto.setContent(boardCommentUpdateDto.getContent());
+        boardCommentDto.setCreateTime(new Timestamp(new Date().getTime()));
+        boardCommentDto.setUpdateTime(new Timestamp(new Date().getTime()));
+        // 추후 jwt 활용
+        boardCommentDto.setUserId("userId");
+        boardCommentDto.setNickname("nickName");
+
         BoardCommentDto resultDto = boardCommentService.updateBoardComment(boardCommentDto);
         return ResponseEntity.ok(resultDto);
     }
