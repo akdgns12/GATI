9fe00437e97927d2be954d392dc5037c47c08305
hhj <gkagu12@naver.com>
2023-02-08 17:25:31 +0900

    fileservice completed

--- be/src/main/java/com/family/gati/service/FileService.java
+++ be/src/main/java/com/family/gati/service/FileService.java
@@ -1,25 +1,56 @@
 package com.family.gati.service;
 
-import com.family.gati.dto.FileDto;
-import com.family.gati.entity.FileEntity;
-import com.family.gati.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 public class FileService {
-    private final FileRepository fileRepository;
+    @Value("${upload.dir}")
+    private String uploadDir;
+    public String fileUpload(MultipartFile multipartFile, String location) throws Exception{
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HHmmssSSS");
 
-    public void save(FileDto fileDto){
-        FileEntity fileEntity = new FileEntity(fileDto.getTitle(), fileDto.getUrl());
-        fileRepository.save(fileEntity);
+        //        ubuntu only
+        //        String filePath = uploadDir+"/"+location+multipartFile.getOriginalFilename();
+        //        window only
+        String filePath = uploadDir+"\\"+location+"\\"+date.format(dateTimeFormatter)+"_"+multipartFile.getOriginalFilename();
+        String fileDir = uploadDir+"\\"+location;
+        File file = new File(fileDir);
+        if(!file.exists()){
+            file.mkdir();
+            file = new File(filePath);
+        }else{
+            file = new File(filePath);
+        }
+        multipartFile.transferTo(file);
+        return filePath;
     }
 
-    public List<FileEntity> getFiles(){
-        List<FileEntity> all = fileRepository.findAll();
-        return all;
+    public boolean fileDelete(String path) throws Exception{
+
+        File file = new File(path);
+        if(!file.exists()){
+            return false;
+        }else{
+            file.delete();
+            return true;
+        }
     }
+
 }
