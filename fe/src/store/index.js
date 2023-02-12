fff5cf22cf489a20d80c607ce1a9577527519ee8
bkkmw <roboticts49@gmail.com>
2023-02-12 19:46:09 +0900

    [Fix] errors while resolving conflicts

--- fe/src/store/index.js
+++ fe/src/store/index.js
@@ -5,7 +5,7 @@ import schedule from "./Schedule/schedule.js";
 import user from "./User/user.js";
 import "font-awesome/css/font-awesome.min.css";
 import city from "./GoTogether/city.js";
-import picsTg from "./PicsTogether/picsTg.jsx";
+import picsTg from "./PicsTogether/picsTg.js";
 import photoBoard from "./PhotoBoard/photoBoard.jsx";
 
 const rootReducer = combineReducers({
