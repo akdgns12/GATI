28c23c1a6f01ad7632f6376185ad4c2c1e523724
bkkmw <roboticts49@gmail.com>
2023-02-12 21:13:45 +0900

    [Feat] Family List

--- fe/src/components/SideBar/Family.jsx
+++ fe/src/components/SideBar/Family.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import httpClient from "../../utils/axios";
+
+import FamilyItem from "./FamilyItem";
 
 export default function Family() {
+  const { userId } = useSelector((state) => state.user.loginUser);
+  const [groupList, setGroupList] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const userId_ = "akdgns12";
+    httpClient
+      .get(`/family/list/${userId_}`)
+      .then(({ data }) => {
+        console.log(data);
+        setGroupList(data.familyList);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  useEffect(() => {
+    if (groupList != null) {
+      // console.log(groupList);
+      setLoaded(true);
+    }
+  }, [groupList]);
+
   return (
-    <div>Family</div>
-  )
+    <Box>
+      <div>Family</div>
+      {loaded &&
+        groupList.length > 0 &&
+        groupList.map((group, index) => {
+          return <FamilyItem key={index} group={group} />;
+        })}
+    </Box>
+  );
 }
